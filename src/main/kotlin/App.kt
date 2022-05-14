31d2819ece5d19863c26ff10f40488b13b4e6f0f
Renan Willian <renanwrs@gmail.com>
2022-05-14 17:09:57 -0300

    More Components

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -21,18 +21,16 @@ data class Video(
     val videoUrl: String
 )
 
-val unwatchedVideos = listOf(
-    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
-    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
-    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg"),
-)
-
-val watchedVideos = listOf(
-    Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
-)
-
 val App = FC<Props> {
     var currentVideo: Video? by useState(null)
+    var unwatchedVideos: List<Video> by useState(listOf(
+        Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+        Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+        Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+    ))
+    var watchedVideos: List<Video> by useState(listOf(
+        Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+    ))
 
     h1 { +"KotlinConf Explorer" }
     div {
@@ -50,14 +48,20 @@ val App = FC<Props> {
             onSelectVideo = { video -> currentVideo = video }
         }
     }
-    div {
-        css {
-            position = Position.absolute
-            top = 10.px
-            right = 10.px
+    currentVideo?.let { curr ->
+        VideoPlayer {
+            video = curr
+            unwatchedVideo = curr in unwatchedVideos
+            onWatchedButtonPressed = {
+                if (video in unwatchedVideos) {
+                    unwatchedVideos = unwatchedVideos - video
+                    watchedVideos = watchedVideos + video
+                } else {
+                    watchedVideos = watchedVideos - video
+                    unwatchedVideos = unwatchedVideos + video
+                }
+            }
         }
-
-        h3 { +"John Doe: Building and breaking things" }
-        img { src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" }
     }
+
 }
